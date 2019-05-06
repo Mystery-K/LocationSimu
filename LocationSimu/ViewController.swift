2f8351ccb50e4c16d448340faaa87fdb14b0eac6
kongwei.fe <kongwei.fe@bytedance.com>
2019-05-06 12:45:12 +0800

    Initial Commit

---
+++ LocationSimu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationSimu
+//
+//  Created by kongwei on 2019/5/6.
+//  Copyright © 2019 Mystery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
